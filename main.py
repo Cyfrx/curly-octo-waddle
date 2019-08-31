d2a41e4e27f368f27adec5547b12bbeee081ab1b
Cyfrx <48665732+Cyfrx@users.noreply.github.com>
2019-08-31 16:35:08 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 from papirus import PapirusComposite
 
@@ -10,12 +11,13 @@ class PiDisplay:
         self.display = PapirusComposite(False)
         self.display.AddText("testing", 0, 0, size=12, Id="lineOne")
         # self.display.AddImg(os.path.join(DIRECTORY, 'test', 'images', self.placeholder),0,0, (100,100), Id = "prototype")
-        self.display.AddImg(os.path.join(DIRECTORY,self.placeholder), 1, 63, (32, 32), Id="ForecastIconOne")
+        # self.display.AddImg(os.path.join(), 1, 63, (32, 32), Id="ForecastIconOne")
         self.display.WriteAll()
 
 PI = PiDisplay()
 
 
 PI.update()
-# while True:
-#     PI.update()
+while True:
+    sleep(10)
+    PI.update()
