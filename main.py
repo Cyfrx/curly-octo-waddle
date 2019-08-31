dd3bf749f78a0ccc4a9b54efb33a9449bcf6d62f
Cyfrx <48665732+Cyfrx@users.noreply.github.com>
2019-08-31 16:28:53 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ class PiDisplay:
     def update(self):
         self.display = PapirusComposite(False)
         self.display.AddText("testing", 0, 0, size=12, Id="lineOne")
-        self.display.WriteAll()
         # self.display.AddImg(os.path.join(DIRECTORY, 'test', 'images', self.placeholder),0,0, (100,100), Id = "prototype")
         self.display.AddImg(os.path.join(DIRECTORY,self.placeholder), 1, 63, (32, 32), Id="ForecastIconOne")
+        self.display.WriteAll()
 
 PI = PiDisplay()
 
