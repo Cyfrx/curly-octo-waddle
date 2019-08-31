40fe243d276fe5ad9871a4318ce7db1c177f3ce4
Cyfrx <48665732+Cyfrx@users.noreply.github.com>
2019-08-31 16:12:09 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import os
 
 from papirus import PapirusComposite
 
+DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
 class PiDisplay:
 
     def update(self):
         self.display = PapirusComposite(False)
         self.display.AddText("testing", 0, 0, size=12, Id="lineOne")
         self.display.WriteAll()
+        # self.display.AddImg(os.path.join(DIRECTORY, 'test', 'images', self.placeholder),0,0, (100,100), Id = "prototype")
+        self.display.AddImg(os.path.join(
+            DIRECTORY, 'images', 'weather', self.placeholder), 1, 63, (32, 32), Id="ForecastIconOne")
 
 PI = PiDisplay()
 
