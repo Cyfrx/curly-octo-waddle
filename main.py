9d863120f0834ae5651ff11fe20e15bce5d7f4f8
Cyfrx <48665732+Cyfrx@users.noreply.github.com>
2019-08-31 13:35:54 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import papirus
+from papirus import PapirusComposite
 
+class PiDisplay:
+
+    def initialize_display(self):
+        self.display.WriteAll()
+
+
+    def update(self):
+        self.display = PapirusComposite(False)
+        self.initialize_display()
+        self.display.AddText("testing")
+
+PI = PiDisplay()
+
+while True:
+    PI.update()
