595e763da8398ce3a5e7a0f97db7500153fce4f1
Cyfrx <48665732+Cyfrx@users.noreply.github.com>
2019-08-31 18:21:04 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ class PiDisplay:
         response = requests.get(complete_url)
         x = response.json()
         y = x["main"]
-        # weath = x["weather"]
-        # z = weath[2]
+        w = x["weather"]
+        desc = w["description"]
         current_temperature = y["temp"]
         americanunits = (9/5) * (current_temperature - 273.15) + 32
         current_temperature = americanunits
